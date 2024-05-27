291c6de6519b80093b5ac9cbaf1aa8aba2d75fa2
BluehatAspie <ehmdiagne0530@gmail.com>
2024-05-26 20:44:20 -0400

    set up basic ui for component selector

--- src/jupiter/ui/ComponentSelector.java
+++ src/jupiter/ui/ComponentSelector.java
@@ -2,6 +2,10 @@ package jupiter.ui;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JButton;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 /**
  * ComponentSelector
@@ -9,13 +13,24 @@ import javax.swing.JFrame;
 public class ComponentSelector extends JDialog {
 
     public ComponentSelector(JFrame parent) {
+        /* setting JDialog owner */
         super(parent, "Select a Component");
         
+        /* configuration */
         this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(400, 400);
+        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        this.setSize(450, 450);
         this.setResizable(false);
         this.setVisible(true);
+
+        /* adding options */
+        String[] componentNames = {"Battery", "Resistor", "Wire"};
+        for (String name : componentNames) {
+            JButton button = new JButton(name);
+            button.setFocusable(false);
+            button.setPreferredSize(new Dimension(100, 100));
+            this.add(button);
+        }
     }
-    
+
 }
