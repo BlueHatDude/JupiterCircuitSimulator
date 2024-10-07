fb5712077ec20f15dd4084ebfae2455f52df9216
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-10-07 11:45:00 -0400

    added exception

---
+++ src/main/java/jupiter/exceptions/IncompletePath.java
@@ -0,0 +1,16 @@
+package jupiter.exceptions;
+
+/**
+ * IncompletePath
+ */
+public class IncompletePath extends Exception {
+
+    public IncompletePath() {
+        super();
+    }
+
+    public IncompletePath(String message) {
+        super(message);
+    }
+    
+}
