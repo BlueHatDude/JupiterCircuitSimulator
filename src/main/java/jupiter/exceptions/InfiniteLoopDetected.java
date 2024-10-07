a6ed333ebf36b4e0082b60f9aee74cf9536f289c
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-10-07 11:54:10 -0400

    added exception

---
+++ src/main/java/jupiter/exceptions/InfiniteLoopDetected.java
@@ -0,0 +1,16 @@
+package jupiter.exceptions;
+
+/**
+ * InfiniteLoopDetected
+ */
+public class InfiniteLoopDetected extends Exception {
+
+    public InfiniteLoopDetected() {
+        super();
+    }
+    
+    public InfiniteLoopDetected(String message) {
+        super(message);
+    }
+
+}
